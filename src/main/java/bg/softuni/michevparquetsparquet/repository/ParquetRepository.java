c884186d3e854d86900ab351a611edf8c7207acc
Martin <marti_1015@abv.bg>
2024-07-18 13:20:37 +0300

    Fixing bug

--- src/main/java/bg/softuni/michevparquetsparquet/repository/ParquetRepository.java
+++ src/main/java/bg/softuni/michevparquetsparquet/repository/ParquetRepository.java
@@ -5,10 +5,11 @@ import bg.softuni.michevparquetsparquet.model.enums.ModelName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ParquetRepository extends JpaRepository<Parquet, Long> {
     Optional<Parquet> findByName (String name);
-    Optional<Parquet> findAllByModelModelName (ModelName modelName);
+    List<Parquet> findAllByModelModelName (ModelName modelName);
 }
